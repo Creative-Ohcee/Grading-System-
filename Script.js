f2008f654a39ae7268b72ea60e47f6a1e98b9f3b
Creative-Ohcee <adelekeoluwadara0@gmail.com>
2023-04-05 11:07:07 +0100

    New commit

--- Script.js
+++ Script.js
@@ -1,17 +1,17 @@
 function checkGrade() {
     if (grade.value == "") {
-        alert("Enter a valid Grade");
+        result.innerHTML = "Enter a valid Grade"
     } else if (grade.value <= 44) {
-        alert("F you have failed");
+        result.innerHTML = "F you have failed"
     } else if (grade.value >= 44 && grade.value <= 49) {
-        alert("you scored E");
+        result.innerHTML = "you scored E"
     } else if (grade.value >= 50 && grade.value <= 54) {
-        alert("you scored D");
+        result.innerHTML = "you scored D"
     } else if (grade.value >= 54 && grade.value <= 59) {
-        alert("you scored C");
+        result.innerHTML = "you scored C"
     } else if (grade.value >= 60 && grade.value <= 69) {
-        alert("you scored B");
+        result.innerHTML = "you scored B"
     } else {
-        alert("you scored A");
+        result.innerHTML = "you scored A"
     }
 }
